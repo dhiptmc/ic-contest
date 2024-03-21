fff5a739b5e1e120726fa7d50b6c5208cd232ad3
俞杉麒 Yu, Shan-Chi <p78111519@gs.ncku.edu.tw>
2024-03-21 16:21:11 +0800

    Update SME.v

--- 2020/SME.v
+++ 2020/SME.v
@@ -8,7 +8,7 @@ output reg match;
 output reg [4:0] match_index;
 output reg valid;
 
-reg [7:0] string [33:0];
+reg [7:0] stringg [33:0];
 reg [5:0] stringcounter; 
 reg [5:0] stringcounterini;
 
@@ -38,7 +38,7 @@ begin
 
     for( i = 0; i <= 33; i = i + 1)
     begin
-      string[i] <= 8'h20; // initial spaces
+      stringg[i] <= 8'h20; // initial spaces
     end
     for( i = 0; i <= 7; i = i + 1)
     begin
@@ -52,7 +52,7 @@ begin
       match <= 1'b0;
       match_index <= 5'b0;
       valid <= 1'b0;
-      string[stringcounterini] <= chardata;
+      stringg[stringcounterini] <= chardata;
       stringcounter <= stringcounterini + 1;
       stringcounterini <= stringcounterini + 1;
     end
@@ -71,7 +71,7 @@ begin
     begin
       stringcounterini <= 6'd1;
       patterncounterini <= 6'b0;
-      string[stringcounter] <= 8'h20;
+      stringg[stringcounter] <= 8'h20;
       pattern[patterncounter] <= 8'b0;
 
       //for( i = stringcounter; i <= 33; i = i + 1)
@@ -83,13 +83,13 @@ begin
         //pattern[i] <= 8'b0;
       //end
 
-      if( pattern[0] == 8'h5e && pattern[patterncounter - 1] == 8'h24 ) // ^  $
+      if( (pattern[0] == 8'h5e) && (pattern[patterncounter - 1] == 8'h24) ) // ^  $
       begin
         if( testcounter <= stringcounter - ( patterncounter - 1 ) ) // check boundary
         begin
-          if( string[testcounter] == 8'h20 && string[testcounter + patterncounter - 1] == 8'h20 ) //  20 (something) 20
+          if( (stringg[testcounter] == 8'h20) && (stringg[testcounter + patterncounter - 1] == 8'h20) ) //  20 (something) 20
           begin
-            if( string[testcounter + 1 + testpatternindex] == pattern[testpatternindex + 1] || pattern[testpatternindex + 1] == 8'h2e ) //fit
+            if( (stringg[testcounter + 1 + testpatternindex] == pattern[testpatternindex + 1]) || (pattern[testpatternindex + 1] == 8'h2e) ) //fit
             begin
               if( testpatternindex == patterncounter - 3 )
               begin
@@ -133,9 +133,9 @@ begin
       begin
         if( testcounter <= stringcounter - patterncounter ) // check boundary
         begin
-          if( string[testcounter] == 8'h20 ) //  20 (something)
+          if( stringg[testcounter] == 8'h20 ) //  20 (something)
           begin
-            if( string[testcounter + 1 + testpatternindex] == pattern[testpatternindex + 1] || pattern[testpatternindex + 1] == 8'h2e ) //fit
+            if( (stringg[testcounter + 1 + testpatternindex] == pattern[testpatternindex + 1]) || (pattern[testpatternindex + 1] == 8'h2e) ) //fit
             begin
               if( testpatternindex == patterncounter - 2 )
               begin
@@ -179,9 +179,9 @@ begin
       begin
         if( testcounter <= stringcounter - ( patterncounter - 1 ) ) // check boundary
         begin
-          if( string[testcounter + patterncounter ] == 8'h20 ) //  (something) 20
+          if( stringg[testcounter + patterncounter ] == 8'h20 ) //  (something) 20
           begin
-            if( string[testcounter + 1 + testpatternindex] == pattern[testpatternindex] || pattern[testpatternindex] == 8'h2e ) //fit
+            if( (stringg[testcounter + 1 + testpatternindex] == pattern[testpatternindex]) || (pattern[testpatternindex] == 8'h2e) ) //fit
             begin
               if( testpatternindex == patterncounter - 2 )
               begin
@@ -225,7 +225,7 @@ begin
       begin
         if( testcounter <= stringcounter - patterncounter ) // check boundary
         begin
-          if( string[testcounter + 1 + testpatternindex] == pattern[testpatternindex] || pattern[testpatternindex] == 8'h2e ) //fit
+          if( (stringg[testcounter + 1 + testpatternindex] == pattern[testpatternindex]) || (pattern[testpatternindex] == 8'h2e) ) //fit
           begin
             if( testpatternindex == patterncounter - 1 )
             begin
